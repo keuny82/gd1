a17c7f94c125edb226cf335d43ada1f963aa505b
keuny82 <dklee22@gmail.com>
2025-04-14 23:56:58 +0900

    4th commit

--- Script/cardmanager.gd
+++ Script/cardmanager.gd
@@ -12,7 +12,7 @@ var screen_size
 var card_being_dragged
 var is_hovered_on_card
 var player_hand_reference
-
+var played_moster_card_this_turn = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -37,16 +37,21 @@ func finish_drag():
 	card_being_dragged.scale = Vector2(CARD_BIGGER_SCALE, CARD_BIGGER_SCALE)
 	var card_slot_found = raycast_check_for_card_slot()
 	if card_slot_found and not card_slot_found.card_in_slot:
-		# 카드를 슬롯에 끼우면
-		card_being_dragged.scale = Vector2(CARD_SMALLER_SCALE, CARD_SMALLER_SCALE)
-		card_being_dragged.z_index = -1
-		card_being_dragged.card_slot_card_is_in = card_slot_found
-		player_hand_reference.remove_card_from_hand(card_being_dragged)
-		card_being_dragged.position = card_slot_found.position
-		card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
-		card_slot_found.card_in_slot = true
-	else:
-		player_hand_reference.add_card_to_hand(card_being_dragged, DEFAULT_CARD_DRAW_SPEED)
+		if card_being_dragged.card_type == card_slot_found.card_slot_type:
+			# cardtype 과 slottype 이 같은곳에 카드를 끼우면
+			if !played_moster_card_this_turn:
+				played_moster_card_this_turn = true
+				card_being_dragged.scale = Vector2(CARD_SMALLER_SCALE, CARD_SMALLER_SCALE)
+				card_being_dragged.z_index = -1
+				is_hovered_on_card = false
+				card_being_dragged.card_slot_card_is_in = card_slot_found
+				player_hand_reference.remove_card_from_hand(card_being_dragged)
+				card_being_dragged.position = card_slot_found.position
+				card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
+				card_slot_found.card_in_slot = true
+				card_being_dragged = null				
+				return
+	player_hand_reference.add_card_to_hand(card_being_dragged, DEFAULT_CARD_DRAW_SPEED)
 	card_being_dragged = null
 	
 
